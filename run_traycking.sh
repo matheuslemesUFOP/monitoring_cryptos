1798a6736907967d684a9661e11f6e6a73d94ed8
Matheus Lemes <matheus.faria@aluno.ufop.edu.br>
2022-12-22 10:46:59 -0300

    update bash script

--- run_traycking.sh
+++ run_traycking.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/matheus_enacom/.virtualenvs/monitoring_cryptos/
 source bin/activate
-python3 /home/matheus_enacom/PycharmProjects/monitoring_cryptos/crypto_tracking.py
+cd /home/matheus_enacom/PycharmProjects/monitoring_cryptos/
+python3 crypto_tracking.py
